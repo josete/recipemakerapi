27e7a3bb14c86b719d89bd7334a29a0cfd402c25
Jose <josecaramor1@hotmail.com>
2021-02-13 21:01:10 +0100

    guardar receta

---
+++ index.js
@@ -0,0 +1,33 @@
+var path = require("path");
+var express = require('express');
+var bodyParser = require("body-parser");
+var fs = require("fs");
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+var options = process.argv.slice(2);
+
+app.post("/recipe", function (req, res) {
+    var date = new Date(Date.now());
+    var dateString = date.getFullYear()+"-"+("0" + date.getMonth()).slice(-2)+"-"+("0" + date.getDay()).slice(-2);
+    var fileName = dateString+"-"+req.body.receta.title;
+    var recipeText = "--- \n layout: post \n title: \""+req.body.receta.title+"\" \n date: "+dateString+"\n categories: "+req.body.receta.category+"\n ingredientes: \n";
+    req.body.receta.ingredientes.forEach(ingredient => {
+        recipeText += ingredient+"\n";
+    });
+    recipeText+="pasos: \n";
+    req.body.receta.pasos.forEach(paso => {
+        recipeText += paso+"\n";
+    });
+    recipeText += "---";
+    if (options.length == 0){
+        fs.writeFileSync(fileName+".md",recipeText);
+    }else{
+        fs.writeFileSync(path.join(options[0],fileName+".md"),recipeText);
+    }
+    res.sendStatus(200);
+});
+
+app.listen(3001);
